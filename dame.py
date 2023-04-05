8048a1187dd244f7b57ccc8582a6d487924bf597
Szabi <sszabbi@gmail.com>
2023-04-05 17:41:19 +0200

    Add basic move validity checker

--- dame.py
+++ dame.py
@@ -33,10 +33,23 @@ class Dame:
                 self.board[x,y] = 2
                 self.black_pieces.append((x,y))
 
-        self.draw_as = {
+        # Used when the drawing board
+        self.draw_as = { 
             0: '_', # Empty squares
             1: 'W', # White pieces 
             2: 'B', # Black pieces
+        }
+
+        # Used when moving pieces
+        self.char_to_col = {
+            'a':0,
+            'b':1,
+            'c':2,
+            'd':3,
+            'e':4,
+            'f':5,
+            'g':6,
+            'h':7
         }
 
     def print_board(self):
@@ -54,11 +67,43 @@ class Dame:
         print("    a b c d e f g h")
         print()
 
+    def is_move_valid(self, fro, to, for_white):
+        '''
+            Examines the validity of a player moving from "fro" to "to".
+
+            Player is white iff for_white is True
+            fro and to are strings in the format of "{letter}{number}" as read from the board
+            e.g.: "a8" for the upper-left corner.
+        '''
+
+        # Horrible input check
+        if len(fro) != 2 or len(to) != 2: 
+            print("Something went terribly wrong here.")
+            return False
+        
+
+        # Making arguments usable
+        xf,yf = fro.split()
+        xf = self.char_to_col.get(xf, -1) # useful column number
+        yf -= 1 # indexed from 0
+
+        xto,yto = to.split()
+        xto = self.char_to_col.get(xto, -1) # useful column number
+        yto -= 1 # indexed from 0
+
+        # Bad input check
+        for coord in [xf,yf,xto,yto]:
+            if coord not in range(8):
+                print("I don't like that.")
+                return False
+            
+        return True
+
+
 
 
 if __name__ == "__main__":
 
     dame = Dame()
     dame.print_board()
-    print("White pieces at", *dame.white_pieces, sep= " ")
-    print("Black pieces at", *dame.black_pieces, sep= " ")
+    print(dame.is_move_valid('a1','b2', for_white=True))
